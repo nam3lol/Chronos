eb89e5c76589979f235b154ea46f755c9295d09e
nam3 <defnotnam3@gmail.com>
2026-06-17 21:28:53 -0400

    Added source to GitHub repo.

---
+++ Classes/AnimLib.cs
@@ -0,0 +1,114 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+class AnimLib
+{
+    public static IEasingFunction Quad { get; set; } = new QuadraticEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Sine { get; set; } = new SineEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Back { get; set; } = new BackEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Bounce { get; set; } = new BounceEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Circle { get; set; } = new CircleEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Cubic { get; set; } = new CubicEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Elastic { get; set; } = new ElasticEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Exponential { get; set; } = new ExponentialEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Power { get; set; } = new PowerEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Quartic { get; set; } = new QuarticEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static IEasingFunction Quintic { get; set; } = new QuinticEase
+    {
+        EasingMode = EasingMode.EaseInOut
+    };
+
+    public static void ObjectMove(DependencyObject Object, Thickness Get, Thickness Set, int MilliSeconds = 750)
+    {
+        Storyboard Board = new Storyboard();
+        ThicknessAnimation Animation = new ThicknessAnimation
+        {
+            EasingFunction = Quad,
+            From = Get,
+            To = Set,
+            AutoReverse = false,
+            Duration = new Duration(TimeSpan.FromMilliseconds(MilliSeconds))
+        };
+
+        Storyboard.SetTarget(Animation, Object);
+        Storyboard.SetTargetProperty(Animation, new PropertyPath(FrameworkElement.MarginProperty));
+
+        Board.Children.Add(Animation);
+        Board.Begin();
+    }
+
+    public static void Fade(DependencyObject Object, double From = 0, double To = 1, int MilliSeconds = 500)
+    {
+        Storyboard Board = new Storyboard();
+        DoubleAnimation Animation = new DoubleAnimation()
+        {
+            From = From,
+            To = To,
+            AutoReverse = false,
+            Duration = new Duration(TimeSpan.FromMilliseconds(MilliSeconds))
+        };
+
+        Storyboard.SetTarget(Animation, Object);
+        Storyboard.SetTargetProperty(Animation, new PropertyPath("Opacity", 1));
+
+        Board.Children.Add(Animation);
+        Board.Begin();
+    }
+
+    public static void DoubleAnimation(DependencyObject Object, string Property, double From = 0, double To = 1, int MilliSeconds = 500)
+    {
+        Storyboard Board = new Storyboard();
+        DoubleAnimation Animation = new DoubleAnimation()
+        {
+            From = From,
+            To = To,
+            AutoReverse = false,
+            Duration = new Duration(TimeSpan.FromMilliseconds(MilliSeconds))
+        };
+
+        Storyboard.SetTarget(Animation, Object);
+        Storyboard.SetTargetProperty(Animation, new PropertyPath(Property, 1));
+
+        Board.Children.Add(Animation);
+        Board.Begin();
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,247 @@
+﻿using Chronos.Properties;
+using ICSharpCode.AvalonEdit;
+using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Chronos
+{
+    public partial class MainWindow : Window
+    {
+        Geometry geometry_Maximize = Application.Current.TryFindResource("Maximize") as Geometry;
+        Geometry geometry_Restore = Application.Current.TryFindResource("Restore") as Geometry;
+
+        Geometry geometry_SidebarEnabled = Application.Current.TryFindResource("Sidebar_Enabled") as Geometry;
+        Geometry geometry_SidebarDisabled = Application.Current.TryFindResource("Sidebar_Disabled") as Geometry;
+
+        Geometry geometry_BottombarEnabled = Application.Current.TryFindResource("Bottombar_Enabled") as Geometry;
+        Geometry geometry_BottombarDisabled = Application.Current.TryFindResource("Bottombar_Disabled") as Geometry;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Opacity = 0;
+            grid_BorderGrid.Opacity = 0;
+            grid_BorderGrid.Visibility = Visibility.Collapsed;
+            grid_Splash.Opacity = 0;
+
+            SplashAnimation();
+
+            UpdateUndoRedoButtons();
+            avalonEdit_TextEditor.TextChanged += TextEditor_TextChanged;
+        }
+
+        public void LoadSettings()
+        {
+
+            ToggleSidebar(Settings.Default.Sidebar);
+            ToggleBottombar(Settings.Default.Bottombar);
+        }
+
+        public void ToggleSidebar(bool enabled)
+        {
+            if (enabled)
+            {
+                border_Sidebar.Visibility = Visibility.Visible;
+                path_Sidebar.Data = geometry_SidebarEnabled;
+                Settings.Default.Sidebar = true;
+            }
+            else
+            {
+                border_Sidebar.Visibility = Visibility.Collapsed;
+                path_Sidebar.Data = geometry_SidebarDisabled;
+                Settings.Default.Sidebar = false;
+            }
+        }
+
+        public void ToggleBottombar(bool enabled)
+        {
+            if (enabled)
+            {
+                border_Bottombar.Visibility = Visibility.Visible;
+                path_Bottombar.Data = geometry_BottombarEnabled;
+                Settings.Default.Bottombar = true;
+            }
+            else
+            {
+                border_Bottombar.Visibility = Visibility.Collapsed;
+                path_Bottombar.Data = geometry_BottombarDisabled;
+                Settings.Default.Bottombar = false;
+            }
+        }
+
+        private void TextEditor_TextChanged(object? sender, EventArgs e)
+        {
+            UpdateUndoRedoButtons();
+        }
+
+        private void Drag(object sender, MouseButtonEventArgs e) => DragMove();
+
+        private void OpenFile()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Title = "Chronos - Open File";
+            dialog.Filter =
+                "Normal Text File (*.txt;)|*.txt|" +
+                "Markdown File (*.md;*.markdown)|*.md;*.markdown|" +
+                "All Files (*.*)|*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                string filePath = dialog.FileName;
+
+                string contents = File.ReadAllText(filePath);
+
+                avalonEdit_TextEditor.Text = contents;
+            }
+        }
+
+        private void SaveFile()
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            dialog.Title = "Chronos - Save File";
+            dialog.Filter =
+                "Normal Text File (*.txt;)|*.txt|" +
+                "Markdown File (*.md;*.markdown)|*.md;*.markdown|" +
+                "All Files (*.*)|*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                string filePath = dialog.FileName;
+
+                string contents = File.ReadAllText(filePath);
+
+                File.WriteAllText(filePath, contents);
+            }
+        }
+
+        private void UpdateUndoRedoButtons()
+        {
+            button_Undo.IsEnabled = avalonEdit_TextEditor.CanUndo;
+            button_Redo.IsEnabled = avalonEdit_TextEditor.CanRedo;
+        }
+
+        private void buttonClick_WindowControls(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                switch (button.Name)
+                {
+                    case "button_Close":
+                        Close();
+                        break;
+                    case "button_Maximize":
+                        WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+                        path_Maximize.Data = WindowState == WindowState.Maximized ? Geometry.Parse(geometry_Restore.ToString()) : Geometry.Parse(geometry_Maximize.ToString());
+                        break;
+                    case "button_Minimize":
+                        WindowState = WindowState.Minimized;
+                        break;
+                }
+            }
+        }
+
+        private void buttonClick_ToolbarControlsRight(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                switch (button.Name)
+                {
+                    case "button_ToggleSidebar":
+                        if (border_Sidebar.Visibility == Visibility.Visible)
+                        {
+                            border_Sidebar.Visibility = Visibility.Collapsed;
+                            path_Sidebar.Data = Geometry.Parse(geometry_SidebarDisabled.ToString());
+                        }
+                        else
+                        {
+                            border_Sidebar.Visibility = Visibility.Visible;
+                            path_Sidebar.Data = Geometry.Parse(geometry_SidebarEnabled.ToString());
+                        }
+                        break;
+                    case "button_ToggleBottombar":
+                        if (border_Bottombar.Visibility == Visibility.Visible)
+                        {
+                            border_Bottombar.Visibility = Visibility.Collapsed;
+                            path_Bottombar.Data = Geometry.Parse(geometry_BottombarDisabled.ToString());
+                        }
+                        else
+                        {
+                            border_Bottombar.Visibility = Visibility.Visible;
+                            path_Bottombar.Data = Geometry.Parse(geometry_BottombarEnabled.ToString());
+                        }
+                        break;
+                    case "button_Settings":
+                        MessageBox.Show("Settings button clicked!");
+                        break;
+                }
+            }
+        }
+
+        private void buttonClick_ToolbarControlsLeft(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                switch (button.Name)
+                {
+                    case "button_OpenFile":
+                        OpenFile();
+                        break;
+                    case "button_SaveFile":
+                        SaveFile();
+                        break;
+                    case "button_Cut":
+                        avalonEdit_TextEditor.Cut();
+                        break;
+                    case "button_Copy":
+                        avalonEdit_TextEditor.Copy();
+                        break;
+                    case "button_Paste":
+                        avalonEdit_TextEditor.Paste();
+                        break;
+                    case "button_Delete":
+                        avalonEdit_TextEditor.Delete();
+                        break;
+                    case "button_Undo":
+                        avalonEdit_TextEditor.Undo();
+                        UpdateUndoRedoButtons();
+                        break;
+                    case "button_Redo":
+                        avalonEdit_TextEditor.Redo();
+                        UpdateUndoRedoButtons();
+                        break;
+                }
+            }
+        }
+
+        public async void SplashAnimation()
+        {
+            grid_Splash.Margin = new Thickness(0, 0, 0, 0);
+            AnimLib.Fade(this, 0, 1, 500);
+            await Task.Delay(1000);
+            AnimLib.Fade(grid_Splash, 0, 1, 500);
+
+            await Task.Delay(2500); // loading code goes here; 1 second delay for demonstration purposes
+
+            AnimLib.Fade(grid_Splash, 1, 0, 500);
+            await Task.Delay(1000);
+            grid_BorderGrid.Visibility = Visibility.Visible;
+            AnimLib.Fade(grid_BorderGrid, 0, 1, 500);
+
+            grid_Splash.Visibility = Visibility.Collapsed;
+            grid_Splash.Margin = new Thickness(999, 999, 999, 999);
+        }
+    }
+}
